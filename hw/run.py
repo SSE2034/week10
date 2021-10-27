9ef457bbc9a00fab850e7b9a8ffca11da1760364
dounghun22 <doughun22@g.skku.edu>
2021-10-27 17:32:32 +0900

    answer

--- hw/run.py
+++ hw/run.py
@@ -2,5 +2,7 @@ import os
 
 file_list = os.listdir("./data")
 
+os.system("./out/*")
+
 for file_name in file_list:
     os.system("./bin/main.out ./data/{} | tee ./out/{}.log".format(file_name, file_name))
